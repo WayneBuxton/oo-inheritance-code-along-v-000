567d542b6253a80b32d0cec6cc0c389b8fae301b
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-24 16:31:46 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -4,7 +4,8 @@ class Car
     attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size,wheel_number)
-
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
 
   def go
